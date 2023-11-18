f288b9e5e7528f3b1243c09b4092ba04f31b6b9e
Rohan Nagar <rohan17nagar@gmail.com>
2023-11-18 13:45:16 +0530

    Banner component creation.

---
+++ jobneedsolutionsfrontend/src/common/Banner/Banner.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Banner.scss";
+const Banner: React.FC = () => {
+  return <div className='banner-container'>User Management</div>;
+};
+
+export default Banner;
